694899038fcb3f0e2bf8d0a6aae338fd296b0daa
hadeel salah <hadeelsalah80@gmail.com>
2020-09-04 18:34:02 +0300

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
-import { Card } from 'antd';
+import React ,{useState, useEffect}from 'react';
+import { PageHeader , Card} from 'antd';
+import api from '../mock_api';
 
 const Post = (props) =>{
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    
+    useEffect(() =>{
+        let post = api[props.id]
+        setTitle(post.title)
+        setContent(post.content)
+    }, [])
     return(
-           <div className="article_container">
-            <Card
-                style={{ marginTop: '16' }}
-                type="inner"
-                title={props.title}
-                extra={<a href="#">More</a>}
-                >
-                <p className="article_content">
-                    {props.content}
+        <div className="post_container">
+        <div className="page_header_container">
+        <PageHeader
+                title={title}
+                style = {{ 
+                    border: '1px solid rgb(235, 237, 240)'
+                  }}
+            />
+        </div>
+        <div className="post_content_container">
+            <Card style={{marginTop: '20px'}}>
+                <p>
+                   {
+                   content.split('\n').map((paragraph, indx) =>{
+                       return <p key={indx}>{paragraph} </p>
+                   })
+                   }
                 </p>
                 
             </Card>
+        </div>
+        </div>
+    )
 
-            </div>
-)
 }
 export default Post;
