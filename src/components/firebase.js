e8199d29c987e1c663e127ff5905efe3874cb279
hadeel salah <hadeelsalah80@gmail.com>
2020-09-07 17:40:10 +0300

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,22 +1,15 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyATYgVXI21Mhps462rafGy4jfIVZ6I0Dj0",
-  authDomain: "crwn-db-2c4aa.firebaseapp.com",
-  databaseURL: "https://crwn-db-2c4aa.firebaseio.com",
-  projectId: "crwn-db-2c4aa",
-  storageBucket: "crwn-db-2c4aa.appspot.com",
-  messagingSenderId: "701302998600",
-  appId: "1:701302998600:web:0adad97009933de8587a7d",
-  measurementId: "G-YPZQ7HHB7H"
+  apiKey: "AIzaSyAyqfkmwg90MuYdgWCNp4xVCsGppJqQ-kw",
+  authDomain: "react-website-blog.firebaseapp.com",
+  databaseURL: "https://react-website-blog.firebaseio.com",
+  projectId: "react-website-blog",
+  storageBucket: "react-website-blog.appspot.com",
+  messagingSenderId: "691893997670",
+  appId: "1:691893997670:web:ea0ba549e160131a151da7"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-var db = firebase.firestore();
-
-
-
-export const auth = firebase.auth();
-export const firestore = firebase.firestore;
-export default db;
+export default firebase;
